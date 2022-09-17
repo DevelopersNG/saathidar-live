c2e6a3dce65101b703cd378572530d224d7d446b
Vikas <developers.ngdigital@gmail.com>
2022-09-17 10:15:46 +0530

    prefernence count

--- Backend/src/main/java/com/sathidar/repository/ShortListsRepository.java
+++ Backend/src/main/java/com/sathidar/repository/ShortListsRepository.java
@@ -13,12 +13,12 @@ public interface ShortListsRepository extends JpaRepository<ShortListsModel, Int
 	@Transactional
 	@Modifying
 	@Query(value="insert into member_shortlists (shortlist_from_id,shortlist_to_id,shortlist_status) values (:shortlist_from_id,:shortlist_to_id,:shortlist_status)",nativeQuery = true)
-	Object addToShortListMember(String shortlist_from_id, String shortlist_to_id, String shortlist_status);
+	int addToShortListMember(String shortlist_from_id, String shortlist_to_id, String shortlist_status);
 
 	
 	@Transactional
 	@Modifying
 	@Query(value="update member_shortlists set shortlist_status= :shortlist_status where shortlist_from_id= :shortlist_from_id and shortlist_to_id= :shortlist_to_id and shortlist_status='add'",nativeQuery = true)
-	Object removeToShortListMember(String shortlist_from_id, String shortlist_to_id, String shortlist_status);
+	int removeToShortListMember(String shortlist_from_id, String shortlist_to_id, String shortlist_status);
 
 }
--- Backend/src/main/java/com/sathidar/service/ShortListService.java
+++ Backend/src/main/java/com/sathidar/service/ShortListService.java
@@ -6,10 +6,10 @@ import com.sathidar.model.ShortListsModel;
 
 public interface ShortListService {
 
-	JSONArray AddToShortLists(ShortListsModel shortListsModel);
+	int AddToShortLists(ShortListsModel shortListsModel);
 
 	JSONArray GetShortListsMember(String member_id);
 
-	JSONArray RemoveToShortLists(ShortListsModel shortListsModel);
+	int RemoveToShortLists(ShortListsModel shortListsModel);
 
 }
